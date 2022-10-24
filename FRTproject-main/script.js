face716f8551afad2756bada0c4d5c86eb9ca99d
javid-4962 <208x1a0591@khitguntur.ac.in>
2022-10-24 20:45:39 +0530

    completed final project for FRT

---
+++ FRTproject-main/script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+const display = document.getElementById('disble')
+
+if(bar){
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
